66887e4d0e540317a186998837ade18187ec32e5
Zenedith <github@zenedith.mailgun.org>
2019-02-07 01:45:49 +0100

    custom_ui

--- home/homeassistant/.homeassistant/www/custom_ui/tasmota-tracker-card.js
+++ home/homeassistant/.homeassistant/www/custom_ui/tasmota-tracker-card.js
@@ -108,6 +108,8 @@ class TasmotaTrackerCard extends HTMLElement {
     const config = this._config;
     const root = this.shadowRoot;
     const card = root.lastChild;
+
+    console.log(hass.states[config.current_version])
     const current_version = hass.states[config.current_version].state;
     this.myhass = hass;
     this.handlers = this.handlers || [];
@@ -122,7 +124,7 @@ class TasmotaTrackerCard extends HTMLElement {
       if (hass.states[sensor]) {
 //        const list = this._filterCards(hass.states[sensor].attributes);
         const sensor_version = hass.states[sensor].state;
-        const name = hass.states[sensor].name;
+        const name = hass.states[sensor].attributes.friendly_name;
         console.log(hass.states[sensor])
 //        const repo = hass.states[sensor].attributes.repo;
 
